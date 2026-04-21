2f69314a4a83bef677c6fbb74eb8fabeb91fee5a
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-04-21 17:51:28 +0700

    menambahkan perilaku

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -6,3 +6,10 @@ class Mahasiswa {
          int nim;
          string nama;
          float nilai;
+        
+            void tampilkanData() {
+                cout << "NIM: " << nim << endl;
+                cout << "Nama: " << nama << endl;
+                cout << "Nilai: " << nilai << endl;
+            }
+};
